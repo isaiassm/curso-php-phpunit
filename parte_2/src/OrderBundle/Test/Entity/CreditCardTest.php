478819dac8dae07717c4b6d99e3ab6a10428d6e6
isaiassm <isaias1113@outlook.com>
2019-08-02 15:06:12 -0300

    Test creditcard em produção

---
+++ parte_2/src/OrderBundle/Test/Entity/CreditCardTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OrderBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+use OrderBundle\Entity\CreditCard;
+
+class CreditCardTest extends TestCase
+{
+    public function CreditReceivedNumber()
+    {
+        
+    }   
+}
