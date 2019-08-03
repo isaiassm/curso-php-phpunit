4cc118c3f747efecf2c591614076e6b7e9967567
isaiassm <isaias1113@outlook.com>
2019-08-02 22:15:26 -0300

     criando teste de credtCard

--- parte_2/src/OrderBundle/Test/Entity/CreditCardTest.php
+++ parte_2/src/OrderBundle/Test/Entity/CreditCardTest.php
@@ -7,8 +7,31 @@ use OrderBundle\Entity\CreditCard;
 
 class CreditCardTest extends TestCase
 {
-    public function CreditReceivedNumber()
+    /**
+     * @test
+     * @dataProvider CreditCardDataprovider
+     */
+    public function CreditReceivedNumber($number, $dateTime, $expectedAllowed )
     {
-        
-    }   
+        $dateTime =  new \DateTime('now');
+        $number = 123456;
+        $creditCard = new CreditCard(
+            $number,
+            $dateTime 
+        );
+
+        $isAllowed = $creditCard->CreditReceivedNumber();
+        $this->assertEquals($expectedAllowed, $isAllowed);
+    } 
+    
+    public function CreditCardDataprovider()
+    {
+        return[
+            'shouldBeAllowedTrue' => [
+                'number' => true,
+                'dateTime' => false,
+                'expectedAllowed' => true
+            ]
+        ];
+    }
 }
